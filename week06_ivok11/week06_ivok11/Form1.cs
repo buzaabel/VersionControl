23c5fb045def384fbf367c8c5bb15f87003e6f12
ABEL BUZA <buza.abel@gmail.com>
2020-10-17 14:34:35 +0200

    commit #3

--- week06_ivok11/week06_ivok11/Form1.cs
+++ week06_ivok11/week06_ivok11/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06_ivok11.MnbServiceReference;
 
 namespace week06_ivok11
 {
@@ -15,7 +16,23 @@ namespace week06_ivok11
         public Form1()
         {
             InitializeComponent();
-            //asd
+            harmasfeladat();
+            
+        }
+
+        public void harmasfeladat()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
