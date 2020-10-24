c318f2199429956055ce182b9e087decae5917e2
ABEL BUZA <buza.abel@gmail.com>
2020-10-24 14:50:55 +0200

    commit#6

--- ivok11_week07/ivok11_week07/Form1.cs
+++ ivok11_week07/ivok11_week07/Form1.cs
@@ -17,6 +17,7 @@ namespace ivok11_week07
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+        Random rng = new Random(1234);
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +25,24 @@ namespace ivok11_week07
             BirthProbabilities = GetBP(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDP(@"C:\Temp\halál.csv");
             dataGridView1.DataSource = BirthProbabilities;
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                
+                for (int i = 0; i < Population.Count; i++)
+                {
+                   
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
         public List<Person> GetPopulation(string csvpath)
         {
