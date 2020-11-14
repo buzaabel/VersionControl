ef6c34a6449ad2a5d8145e1266ca5f45d56dde9d
ABEL BUZA <buza.abel@gmail.com>
2020-11-14 15:03:18 +0100

    commit#11-12

--- ivok11_week8/ivok11_week8/Form1.Designer.cs
+++ ivok11_week8/ivok11_week8/Form1.Designer.cs
@@ -35,11 +35,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.lblNext = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
+            this.mainPanel.Controls.Add(this.button3);
             this.mainPanel.Controls.Add(this.lblNext);
             this.mainPanel.Controls.Add(this.button2);
             this.mainPanel.Controls.Add(this.button1);
@@ -90,6 +92,17 @@
             this.lblNext.TabIndex = 2;
             this.lblNext.Text = "Coming next";
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(34, 71);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 37);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -112,6 +125,7 @@
         private System.Windows.Forms.Label lblNext;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
