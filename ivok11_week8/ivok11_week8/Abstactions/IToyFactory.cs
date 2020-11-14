2c17a215f734e85f8f5c7712b93ce099f218818f
ABEL BUZA <buza.abel@gmail.com>
2020-11-14 14:28:46 +0100

    commit #8-9

---
+++ ivok11_week8/ivok11_week8/Abstactions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ivok11_week8.Abstactions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
